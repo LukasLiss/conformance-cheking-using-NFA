431c0a75ad507245675a44d021a0b57bb343c279
Lukas Liss <lukas.liss@rwth-aachen.de>
2021-06-01 18:15:54 +0200

    perfomance increase with better nfa data model

--- library/nfa.py
+++ library/nfa.py
@@ -1,12 +1,13 @@
-class Place:
+class place:
     def __init__(self, label):
         self.label = label
+        self.transitions = []
 
 class Transition:
-    def __init__(self, activity, start_Place, end_Place):
+    def __init__(self, activity, start_place, end_place):
         self.activity = activity
-        self.start_Place = start_Place
-        self.end_Place = end_Place
+        self.start_place = start_place
+        self.end_place = end_place
 
 class Nfa:
     def __init__(self, label):
@@ -16,8 +17,8 @@ class Nfa:
         self.start_place = None # here we use the definiton of an NFA with just one start place
         self.end_places = [] # but multiple acepting end places
 
-    def add_Place(self, place, is_start_place = False, is_end_place = False) :
-        #check whether place is a Place
+    def add_place(self, place, is_start_place = False, is_end_place = False) :
+        #check whether place is a place
         #check whether place allready exist
         self.places.append(place)
         if is_start_place :
@@ -26,22 +27,27 @@ class Nfa:
         if is_end_place :
             self.end_places.append(place)
 
-    def remove_Place(self, place) :
-        #check whether place is a Place
+    def remove_place(self, place) :
+        #check whether place is a place
         self.places.remove(place)
 
     def add_Transition(self, transition) :
         #check whether transition is a Transition
         #check whether transition only has places that exist
         #check for duplicate
-        self.transitions.append(transition)
+        for place in self.places :
+            if place == transition.start_place :
+                place.transitions.append(transition)
+                break
 
     def remove_Transition(self, transition) :
         #input checks
-        self.transitions.remove(transition)
+        for place in self.places :
+            if place == transition.start_place :
+                place.transitions.remove(transition)
+                break
 
     def is_fitting(self, trace) :
-        #print("Fit with: " + self.start_place.label + " " + ''.join(trace))
         #trace is here just a list of activities
         #check input
 
@@ -49,15 +55,14 @@ class Nfa:
             return (self.start_place in self.end_places)
 
         activity = trace[0]
-        for trans in self.transitions : # performance increase possible by having a reference to connected transition of each place, but increases memory usage
-            if((trans.start_Place == self.start_place) and (trans.activity == activity)) :
-                if( self.__is_subtrace_fitting(trans.end_Place, trace[1:]) == True) :
+        for trans in self.start_place.transitions :
+            if trans.activity == activity :
+                if( self.__is_subtrace_fitting(trans.end_place, trace[1:]) == True) :
                     return True
 
         return False
 
     def __is_subtrace_fitting(self, current_place, trace) :
-        #print("Sub with: " + current_place.label + " " + ''.join(trace))
         #trace is here just a list of activities
         #check input
 
@@ -65,9 +70,9 @@ class Nfa:
             return (current_place in self.end_places)
 
         activity = trace[0]
-        for trans in self.transitions : # performance increase possible by having a reference to connected transition of each place, but increases memory usage
-            if((trans.start_Place == current_place) and (trans.activity == activity)) :
-                if( self.__is_subtrace_fitting(trans.end_Place, trace[1:]) == True) :
+        for trans in current_place.transitions : # performance increase possible by having a reference to connected transition of each place, but increases memory usage
+            if trans.activity == activity :
+                if( self.__is_subtrace_fitting(trans.end_place, trace[1:]) == True) :
                     return True
 
         return False
@@ -75,20 +80,20 @@ class Nfa:
     def log_fittness(self, log):
         # Check for each trace in the log wether the trace is fiting (replayable)
         # return the fraction of traces that are fiting (replayable)
-        return False # Placeholder only
+        return False # placeholder only
 
 
 # Test section
 
 myNFA = Nfa("TestNFA")
-p1 = Place("Greating")
-myNFA.add_Place(p1, True)
-p2 = Place("Start Small Talk")
-myNFA.add_Place(p2)
-p3 = Place("End Small Talk")
-myNFA.add_Place(p3)
-p4 = Place("Good Bye")
-myNFA.add_Place(p4, False, True)
+p1 = place("Greating")
+myNFA.add_place(p1, True)
+p2 = place("Start Small Talk")
+myNFA.add_place(p2)
+p3 = place("End Small Talk")
+myNFA.add_place(p3)
+p4 = place("Good Bye")
+myNFA.add_place(p4, False, True)
 t1 = Transition("a", p1, p2)
 myNFA.add_Transition(t1)
 t2 = Transition("b", p2, p2)
